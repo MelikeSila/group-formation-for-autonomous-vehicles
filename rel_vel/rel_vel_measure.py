6cb928433320088bde810d868dc565b5562b967c
floherian <florian.hechtl@tum.de>
2020-05-22 00:20:29 +0200

    added planning_problem_set argument

--- rel_vel/rel_vel_measure.py
+++ rel_vel/rel_vel_measure.py
@@ -2,17 +2,17 @@ from .rel_vel_group import calc_rel_vel_group
 import commonroad.planning.planning_problem as planning_problem
 
 
-def rel_vel_measure(id_set):
-    # uses the IDs of the vehicles to get the states of the vehicles and than
+def rel_vel_measure(planning_problem_set, id_set):
+    # uses the IDs of the vehicles to get the states of the vehicles and then
     # uses calc_rel_vel_group to get rel_vel
     state_list = []
-    print(id_set)
+    
     # transform ID_list to state_list
     for x in id_set:
-        # print(x)
-        prob = planning_problem.PlanningProblemSet.find_planning_problem_by_id(x)
+        
+        prob = planning_problem_set.find_planning_problem_by_id(x)
         state_list.append(prob.initial_state)
-        print(state_list)
+       
 
     # calculate velocity measure
     vel_measure = calc_rel_vel_group(state_list)
