0bae35d6a9b354e8e09696b63f077ba0372a3468
floherian <florian.hechtl@tum.de>
2020-05-22 00:20:01 +0200

    removed print-functions

--- rel_vel/rel_vel_group.py
+++ rel_vel/rel_vel_group.py
@@ -9,7 +9,7 @@ def calc_rel_vel_group(state_list):
     sum_rel_vel = 0
     for i in range(0, group_size):
         for j in range(i, group_size):
-            sum_rel_vel = sum_rel_vel + rel_vel_vehicle.rel_vel_2_vehicles(i, j)
+            sum_rel_vel = sum_rel_vel + rel_vel_vehicle.rel_vel_2_vehicles(state_list[i], state_list[j])
 
     # normalize relative velocity with group size
     rel_vel_group = sum_rel_vel / (group_size * (group_size - 1))
