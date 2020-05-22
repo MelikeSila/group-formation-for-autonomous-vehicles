aa71890c396983e1d50bceff2f5ff44ecc9e0eb4
Melike <melike.keskin@tum.de>
2020-05-22 11:56:16 +0200

    error of sample fixed

--- GraphBasedDistanceMeasure.py
+++ GraphBasedDistanceMeasure.py
@@ -20,7 +20,7 @@ def CreateLaneletGraph(lanelets):
     for lanelet in lanelets:
         i = lanelet.lanelet_id
         L = nx.DiGraph()
-        L.add_nodes_from([2,3])
+        L.add_nodes_from([1,2])
         L.add_edges_from([(3, 4), (4, 5)])
         G.add_node(i, graph = L)  # add to G the graph of lanelet with id i
     return G
@@ -32,9 +32,12 @@ Sample Using:
 #adj_left=None, adj_left_same_direction=None, adj_right=None, ad_right_same_direction=None, 
 #predecessor, succesor
 #line_marking_right_vertices
+import networkx as nx
 lanelets = scenario.lanelet_network.lanelets
 G = CreateLaneletGraph(lanelets)
-#nx.draw(G.nodes[1]['graph']) #for reaching the graph of lanelet with id i
-nx.draw(G)
+plt.subplot(121)
+nx.draw(G.nodes[1]['graph'], with_labels=True, font_weight='bold') #for reaching the graph of lanelet with id i
+plt.subplot(122)
+nx.draw(G,with_labels=True, font_weight='bold')
 plt.show()
 """
