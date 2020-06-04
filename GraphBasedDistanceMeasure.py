873f21178967f95a7bb529b7b20379ab660f6683
Melike <melike.keskin@tum.de>
2020-06-04 13:48:00 +0200

    adjacent lanelets added to main graph G
    adjacent of the lanelets added to the graph G for using in the future distance measure

--- GraphBasedDistanceMeasure.py
+++ GraphBasedDistanceMeasure.py
@@ -4,7 +4,7 @@ def v( obs):
     return obs.initial_state.position
 
 """
-Sample using of v( obs):
+Sample usage of v( obs):
 
 obstacles = scenario.obstacles
 for o in obstacles:
@@ -29,14 +29,24 @@ def CreateEdgesBtwnLanelets(l):
     # we can manage previously added edges in here but it can be time waste. 
     #I am Not sure which is more efficient
     # Melike
+    #TODO: add adjacency lanelets
     e = []
+    a = []
     if l.predecessor:
         for p in l.predecessor:
             e.append([p, l.lanelet_id ])        
     if l.successor:
         for s in l.successor:
-            e.append([l.lanelet_id, s])        
-    return e
+            e.append([l.lanelet_id, s])     
+    if l.adj_left_same_direction:
+        a.append(l.adj_left)
+        #e.append(l.lanelet_id, l.adj_left.lanelet_id) #TODO decide do we need to connect adj lanelets
+        #e.append(l.adj_left.lanelet_id, l.lanelet_id)
+    if l.adj_right_same_direction:
+        a.append(l.adj_right)
+        #e.append(l.lanelet_id, l.adj_right.lanelet_id)
+        #e.append(l.adj_right.lanelet_id, l.lanelet_id)
+    return e, a
 
 def CreateLaneletGraph(lanelets):
     import networkx as nx
@@ -56,21 +66,70 @@ def CreateLaneletGraph(lanelets):
         L.add_nodes_from(points, distance = lanelet.distance, vertices=lanelet.center_vertices)
         L.add_edges_from(edges)
         #G add nodes and edges
-        edgesLanelet = CreateEdgesBtwnLanelets(lanelet)
-        G.add_node(i, graph = L)  # add to G the graph of lanelet with id i
+        edgesLanelet, adjacent_lanelets = CreateEdgesBtwnLanelets(lanelet)
+        G.add_node(i, adj_lanelet = adjacent_lanelets , graph = L) #adding lanelet graph with id and adjecent lanelet
         G.add_edges_from(edgesLanelet)
     return G
 
 """
-Sample Using of CreateLaneletGraph(lanelets):
+######## Sample Usage ############
+## First run here for testing
+## It include scenario and brief 
+## drawing
+##################################
+
+import os
+import matplotlib.pyplot as plt
+from IPython import display
+
+# import functions to read xml file and visualize commonroad objects
+from commonroad.common.file_reader import CommonRoadFileReader
+from commonroad.visualization.draw_dispatch_cr import draw_object
+
+# generate path of the file to be opened
+#file_path = "ZAM_Tutorial-1_1_T-1.xml"
+#file_path = "ZAM_Tjunction-1_66_T-1.xml"
+file_path = "CHN_Cho-2_1_T-1.xml"
+
+# read in the scenario and planning problem set
+crf = CommonRoadFileReader(file_path)
+scenario, planning_problem_set = crf.open()
+
+# plot the scenario for 40 time step, here each time step corresponds to 0.1 second
+for i in range(0, 40):
+    # uncomment to clear previous graph
+    display.clear_output(wait=True)
+    
+    plt.figure(figsize=(20, 10))
+    # plot the scenario at different time step
+    draw_object(scenario, draw_params={'time_begin': i})
+    # plot the planning problem set
+    draw_object(planning_problem_set)
+    plt.gca().set_aspect('equal')
+    plt.show()
+"""
+
+
+
+
+"""
+########## Sample Usage ###########
+Sample Usage of CreateLaneletGraph(lanelets):
 
 #distance
+# lanelet_id
 #adj_left=None, adj_left_same_direction=None, adj_right=None, ad_right_same_direction=None, 
 #predecessor, succesor
 #line_marking_right_vertices
+
+options1 = {
+    'node_color': 'lightgreen',
+    'node_size': 500,
+    'width': 1,
+}
 options2 = {
     'node_color': 'green',
-    'node_size': 1500,
+    'node_size': 500,
     'width': 1,
 }
 
@@ -78,9 +137,31 @@ import networkx as nx
 lanelets = scenario.lanelet_network.lanelets
 G = CreateLaneletGraph(lanelets)
 plt.subplot(121)
-nx.draw(G.nodes[50195]['graph'], with_labels=True, font_weight='bold') #for reaching the graph of lanelet with id i
+nx.draw_circular(G.nodes[lanelets[0].lanelet_id]['graph'], with_labels=True, font_weight='bold', **options1) #for reaching the graph of lanelet with id i
 plt.subplot(122)
-nx.draw
 nx.draw_circular(G,with_labels=True, font_weight='bold', **options2)
+plt.show()
+"""
+"""
+
+########## Sample Usage ###########
+See more clear graphs and data
+
+print(G.nodes)
+plt.rcParams['figure.figsize'] = (10.0, 10.0) # set default size of plots
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+nx.draw(G,with_labels=True, font_weight='bold', **options2)
+plt.show()
+i = 1
+for lanelet in lanelets:
+    print(lanelet)
+    plt.subplot(1, len(lanelets), i)
+    nx.draw(G.nodes[lanelet.lanelet_id]['graph'], with_labels=True, font_weight='bold', **options1) #for reaching the graph of lanelet with id i
+    print(lanelet.adj_left_same_direction, ": ", lanelet.adj_left)
+    print(lanelet.adj_right_same_direction, ": ", lanelet.adj_right)
+    #if 53 in G.nodes[lanelet.lanelet_id]['adj_lanelet']: #adjacent lanelet control # contain()
+    #    print(True)
+    i = i+1
 plt.show()
 """
