6720dea771d7438fbbebe740d912c8622bab560f
floherian <florian.hechtl@tum.de>
2020-06-18 13:07:44 +0200

    modified rel_vel_measure to make obstacle velocities usable as well

--- rel_vel/rel_vel_measure.py
+++ rel_vel/rel_vel_measure.py
@@ -2,16 +2,16 @@ from .rel_vel_group import calc_rel_vel_group
 import commonroad.planning.planning_problem as planning_problem
 
 
-def rel_vel_measure(planning_problem_set, id_set):
+def rel_vel_measure(planning_problem_set, scenario, group):
     # uses the IDs of the vehicles to get the states of the vehicles and then
     # uses calc_rel_vel_group to get rel_vel
     state_list = []
     
     # transform ID_list to state_list
-    for x in id_set:
+    for x in group:
         
-        prob = planning_problem_set.find_planning_problem_by_id(x)
-        state_list.append(prob.initial_state)
+        #prob = planning_problem_set.find_planning_problem_by_id(x)
+        state_list.append(x.initial_state)
        
 
     # calculate velocity measure
