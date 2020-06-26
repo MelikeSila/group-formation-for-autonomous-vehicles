6de4921b1da7ad4ed5488891d7aba6619b9d0f23
floherian <florian.hechtl@tum.de>
2020-06-27 00:05:13 +0200

    made vehicle list with obstacles only due to not working egovehicle distance measure

--- brute_force/calculate_score.py
+++ brute_force/calculate_score.py
@@ -12,6 +12,8 @@ import GraphBasedDistanceMeasure
 #returns two lists: one with all the possible group assignments, the other with the scores of each assignment
 def get_assignments_scores(scenario, planning_problem_set, ideal_group_size, w_size, w_vel, w_dist):
     veh_list = scenario.dynamic_obstacles
+    #as soon as planning problems work on the distance function use below version
+    #veh_list=list(planning_problem_set.planning_problem_dict.values()) + scenario.dynamic_obstacles
     possible_assignments = all_group_assignments(veh_list)
     assignment_scores =[]
     lanelets = scenario.lanelet_network.lanelets
@@ -23,18 +25,28 @@ def get_assignments_scores(scenario, planning_problem_set, ideal_group_size, w_s
             #calculate scores for each group within this assignemt
             add_size=group_size_measure.group_size_measure(groups,ideal_group_size)
             add_vel=rel_vel_measure.rel_vel_measure(planning_problem_set, scenario, veh_list)
-            #so far only group size
             add_dist=GroupDistance.group_dist(scenario, groups)
+            # adds up all the scores of the groups in one assignment and weighing them
             score=score+(w_size*add_size)+(w_vel*add_vel)+(w_dist*add_dist)
-            #adds up all the scores of the groups in one assignment
+
 
         assignment_scores.append(score)
 
     return possible_assignments, assignment_scores
 
-#returns the groups assignment with the highest score
+#returns the groups assignment with the highest score (and returns the IDs of the objects)
 def get_best_assignment(possible_assignments, assignment_scores):
-    best=assignment_scores.index(max(assignment_scores))
+    best=assignment_scores.index(min(assignment_scores))
     best_assignment=possible_assignments[best]
+    best_assignment_IDs=[]
+    for group in best_assignment:
+        grouplist=[]
+        for veh in group:
+            try:
+                x=veh.obstacle_id
+            except:
+                x=veh.planning_problem_id
+            grouplist.append(x)
+        best_assignment_IDs.append(grouplist)
 
-    return best_assignment
+    return best_assignment_IDs
