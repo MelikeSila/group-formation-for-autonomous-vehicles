9d843b1aae6f805a5b110dd2802b9ea5e3cfb6b0
floherian <florian.hechtl@tum.de>
2020-06-29 12:12:35 +0200

    added file for colorizing groups

---
+++ brute_force/Colorize_groups.py
@@ -0,0 +1,5 @@
+from commonroad.visualization.plot_helper import redraw_obstacles
+
+def colorize_groups(group_assignment_IDS, scenario):
+    for group in group_assignment_IDS:
+        redraw_obstacles(scenario, handles, figure_handle)
