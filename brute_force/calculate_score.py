2a27b920ec668a856d9027353ce99084a3d6d744
floherian <florian.hechtl@tum.de>
2020-06-29 12:54:14 +0200

    added comments for clarity

--- brute_force/calculate_score.py
+++ brute_force/calculate_score.py
@@ -11,9 +11,8 @@ import GraphBasedDistanceMeasure
 
 #returns two lists: one with all the possible group assignments, the other with the scores of each assignment
 def get_assignments_scores(scenario, planning_problem_set, ideal_group_size, w_size, w_vel, w_dist):
-    veh_list = scenario.dynamic_obstacles
-    #as soon as planning problems work on the distance function use below version
-    #veh_list=list(planning_problem_set.planning_problem_dict.values()) + scenario.dynamic_obstacles
+
+    veh_list=list(planning_problem_set.planning_problem_dict.values()) + scenario.dynamic_obstacles
     possible_assignments = all_group_assignments(veh_list)
     assignment_scores =[]
     lanelets = scenario.lanelet_network.lanelets
