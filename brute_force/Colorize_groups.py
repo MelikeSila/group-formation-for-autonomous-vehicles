5a661f238d3d332174fd6ef73fbd495d81183ab9
floherian <florian.hechtl@tum.de>
2020-07-01 00:32:01 +0200

    added color list

--- brute_force/Colorize_groups.py
+++ brute_force/Colorize_groups.py
@@ -1,7 +1,17 @@
 from commonroad.visualization.draw_dispatch_cr import draw_object
+import commonroad.planning as planning
+import matplotlib.patches as patches
 
 def colorize_groups(group_assignment):
+    colors=["'123456'", "'000000'", "'880000'", "'008800'", "'000088'", "'994499'", "'990022'". "'998822'"]
+    i=0
     for group in group_assignment:
-        for veh in group:
 
+        draw_params = {'shape':{'facecolor':color}}
+        for veh in group:
+            if isinstance(veh, planning.planning_problem.PlanningProblem):
+                rect=patches.Rectangle((veh.initial_state.position), 5, 2, veh.initial_state.orientation, linewidth=1, edgecolor=color, facecolor=color )
+
+            else:
+                draw_object(veh, draw_params=draw_params)
 
