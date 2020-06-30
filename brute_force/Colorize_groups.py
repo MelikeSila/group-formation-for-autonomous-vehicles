fec62c8054c995982814f516aab5b964eb18233b
floherian <florian.hechtl@tum.de>
2020-07-01 01:32:28 +0200

    veh colorization depending on object type

--- brute_force/Colorize_groups.py
+++ brute_force/Colorize_groups.py
@@ -2,16 +2,23 @@ from commonroad.visualization.draw_dispatch_cr import draw_object
 import commonroad.planning as planning
 import matplotlib.patches as patches
 
-def colorize_groups(group_assignment):
-    colors=["'123456'", "'000000'", "'880000'", "'008800'", "'000088'", "'994499'", "'990022'". "'998822'"]
-    i=0
-    for group in group_assignment:
 
-        draw_params = {'shape':{'facecolor':color}}
+def colorize_groups(group_assignment, axes):
+    colors = ['#123456', '#000000', '#880000', '#008800', '#000088', '#994499', '#990022', '#998822']
+    i = 0
+    rect = []
+    for group in group_assignment:
+        color = colors[i % 7]
+        draw_params = {'shape': {'facecolor': color}}
         for veh in group:
             if isinstance(veh, planning.planning_problem.PlanningProblem):
-                rect=patches.Rectangle((veh.initial_state.position), 5, 2, veh.initial_state.orientation, linewidth=1, edgecolor=color, facecolor=color )
+                rect[i] = patches.Rectangle((veh.initial_state.position), 7, 3, veh.initial_state.orientation,
+                                            linewidth=1, edgecolor=color, facecolor=color)
+                i = i + 1
 
             else:
                 draw_object(veh, draw_params=draw_params)
 
+        i = i + 1
+    axes.add_patch(rect)
+    return None
