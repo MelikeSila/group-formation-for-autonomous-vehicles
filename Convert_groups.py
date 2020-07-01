700e738c2f049aee00191e3c4c729b7bf3654500
floherian <florian.hechtl@tum.de>
2020-07-01 12:50:51 +0200

    defined what functions should do

--- Convert_groups.py
+++ Convert_groups.py
@@ -1,8 +1,8 @@
 def conv_to_IDs(scenario, planningProblemSet, veh_list):
-
+    #reads in an object list and returns and ID_list
     return ID_list
 
 
 def conv_to_obj(scenario, planningProblemSet, ID_list):
-
+    #reads in an ID_list and returns an objectlist
     return veh_list
