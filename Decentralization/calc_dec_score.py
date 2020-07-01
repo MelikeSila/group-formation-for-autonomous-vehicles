7d0575cd4ad013112dde2ac48cc23c0f92afd87a
floherian <florian.hechtl@tum.de>
2020-07-01 12:55:56 +0200

    added conversion to IDs

--- Decentralization/calc_dec_score.py
+++ Decentralization/calc_dec_score.py
@@ -6,6 +6,7 @@ sys.path.insert(1, path_up)
 from rel_vel import rel_vel_measure
 from group_size_measure import group_size_measure
 import GroupDistance
+import Convert_groups
 
 def calc_array_for_each_vehicle(scenario, planning_problem_set, ideal_group_size, w_size, w_vel, w_dist):
     veh_list = list(planning_problem_set.planning_problem_dict.values()) + scenario.dynamic_obstacles
@@ -15,9 +16,10 @@ def calc_array_for_each_vehicle(scenario, planning_problem_set, ideal_group_size
     for veh in veh_list:
         veh_array=[]
         for other_veh in veh_list:
+            IDs=Convert_groups.conv_to_IDs([veh, other_veh])
             if other_veh!=veh:
                 add_vel=rel_vel_measure.rel_vel_measure(planning_problem_set, scenario, [veh, other_veh])
-                
+
                 add_dist=GroupDistance.group_dist(scenario, IDs)
         array_list.append(veh_array)
 
