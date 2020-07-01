dad8cbbe28e86f9b414364ddb66eb39d8396fb92
floherian <florian.hechtl@tum.de>
2020-07-01 14:34:28 +0200

    removed errors

--- brute_force/Colorize_groups.py
+++ brute_force/Colorize_groups.py
@@ -5,16 +5,16 @@ import matplotlib.patches as patches
 
 def colorize_groups(group_assignment, axes):
     colors = ['#123456', '#000000', '#880000', '#008800', '#000088', '#994499', '#990022', '#998822']
-    i = 0
+    i,j = 0,0
     rect = []
     for group in group_assignment:
-        color = colors[i % 7]
+        color = colors[i % 8]
         draw_params = {'shape': {'facecolor': color}}
         for veh in group:
             if isinstance(veh, planning.planning_problem.PlanningProblem):
-                rect[i] = patches.Rectangle((veh.initial_state.position), 7, 3, veh.initial_state.orientation,
+                rect[j] = patches.Rectangle((veh.initial_state.position), 7, 3, veh.initial_state.orientation,
                                             linewidth=1, edgecolor=color, facecolor=color)
-                i = i + 1
+                j = j + 1
 
             else:
                 draw_object(veh, draw_params=draw_params)
