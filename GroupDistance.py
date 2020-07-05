77075100ada72bb439e07d947065cc07c11f85fa
Melike <melike.keskin@tum.de>
2020-07-05 13:04:51 +0200

    fixed scenario.D argument in graph distance

--- GroupDistance.py
+++ GroupDistance.py
@@ -10,7 +10,7 @@ def group_dist(scenario, objects, ScenarioGraph):
     for i in range(0, group_size):
         for j in range(i, group_size):
 
-            sum_dist = sum_dist + ScenarioGraph.D(objects[i], objects[j])
+            sum_dist = sum_dist + ScenarioGraph.D(objects[i].obstacle_id, objects[j].obstacle_id)
 
     # normalize sum_dist with group size
     if group_size == 1:
