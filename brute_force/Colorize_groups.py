cc6328d295ee75db69a7f545b315b6ec5e1d3758
Melike <melike.keskin@tum.de>
2020-07-16 15:53:27 +0200

    colorize implemented to the groups

--- brute_force/Colorize_groups.py
+++ brute_force/Colorize_groups.py
@@ -4,19 +4,21 @@ import matplotlib.patches as patches
 from matplotlib.collections import PatchCollection
 
 
-def colorize_groups(group_assignment, axes):
-    colors = ['#123456', '#000000', '#880000', '#008800', '#000088', '#994499', '#990022', '#998822']
+def colorize_groups(group_assignment, axes, time):
+    colors = ['#123456', '#998822', '#880000', '#008800', '#000088', '#994499', '#990022', '#000000']
     i = 0
     patch = []
     for group in group_assignment:
         color = colors[i % 8]
-        draw_params = {'shape': {'facecolor': color}}
+        print(color)
+        draw_params = draw_params = {  'time_begin': time,'dynamic_obstacle':{'shape':{'facecolor': color}}}
+        
+        #{'shape': {'facecolor': color}}
         for veh in group:
             if isinstance(veh, planning.planning_problem.PlanningProblem):
                 patch.append(patches.Rectangle(veh.initial_state.position[0]-3.5, veh.initial_state.position[1]-1.5, 7, 3, veh.initial_state.orientation ))
-
-
             else:
+                print(type(veh))
                 draw_object(veh, draw_params=draw_params)
 
         i = i + 1
